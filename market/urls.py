daffa76ea86d172b589e7053bbf116001b38d9f7
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-12-28 14:54:02 -0300

    added url conf for market app

--- market/urls.py
+++ market/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from market.views import HomePageView
+from market.views import HomePageView, SearchResultsView
 
 app_name = 'market'
+
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('search/', SearchResultsView.as_view(), name='search'),
 ]
