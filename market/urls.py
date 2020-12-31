6c5dc42205f450c195a50aab5bfb30b654644b48
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-12-30 22:21:36 -0300

    added house list and apartment list view

--- market/urls.py
+++ market/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from market.views import HomePageView, SearchResultsView
+from market.views import (ApartmentListView, HomePageView, HouseListView,
+                          SearchResultsView)
 
 app_name = 'market'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('search/', SearchResultsView.as_view(), name='search'),
+    path('house/', HouseListView.as_view(), name='house_list'),
+    path('apartment/', ApartmentListView.as_view(), name='apartment_list'),
 ]
