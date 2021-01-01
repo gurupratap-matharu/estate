e6e68f22fdb98625ca2726baa097c70670d99a51
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-01-01 15:32:59 -0300

    added short name for url for search

--- market/urls.py
+++ market/urls.py
@@ -7,7 +7,6 @@ app_name = 'market'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('search/', SearchResultsView.as_view(), name='search'),
     path('house/', HouseListView.as_view(), name='house_list'),
     path('house/<uuid:pk>/', HouseDetailView.as_view(), name='house_detail'),
     path('apartment/', ApartmentListView.as_view(), name='apartment_list'),
