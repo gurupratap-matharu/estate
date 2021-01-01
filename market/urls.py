a2d9c3a5a0dceee085565877df230dd4fe2e7cc1
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-01-01 20:21:26 -0300

    added sellhome endpoint

--- market/urls.py
+++ market/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from market.views import (ApartmentDetailView, ApartmentListView, HomePageView,
-                          HouseDetailView, HouseListView)
+                          HouseDetailView, HouseListView, SellHomeView)
 
 app_name = 'market'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('sell/', SellHomeView.as_view(), name='sell_home'),
     path('house/', HouseListView.as_view(), name='house_list'),
     path('house/<uuid:pk>/', HouseDetailView.as_view(), name='house_detail'),
     path('apartment/', ApartmentListView.as_view(), name='apartment_list'),
