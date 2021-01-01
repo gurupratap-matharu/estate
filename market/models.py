7b05f961b986a0a0457a05a9e2949ead6b081b8e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-01-01 15:32:36 -0300

    added common model fields to base abstract class

--- market/models.py
+++ market/models.py
@@ -9,9 +9,20 @@ class Property(models.Model):
     Abstract Property model which won't be registered in the database.
     Meant as a place holder to store common attributes among all properties
     """
-    square_footage = models.PositiveIntegerField()
+    RENT = 'RT'
+    SALE = 'SL'
+
+    OFFER_CHOICES = [
+        (RENT, 'Rent'),
+        (SALE, 'Sale'),
+    ]
+
+    square_footage = models.PositiveIntegerField(verbose_name='Area in sqft')
     num_bedrooms = models.PositiveSmallIntegerField('Number of bedrooms')
     num_bathrooms = models.PositiveSmallIntegerField('Number of bathrooms')
+    offer = models.CharField(choices=OFFER_CHOICES, default=SALE)
+    price = models.DecimalField(max_digits=10, decimal_places=0)
+
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -35,7 +46,6 @@ class House(Property):
     num_stories = models.PositiveSmallIntegerField('Number of stories')
     garage = models.CharField(max_length=2, choices=GARAGE_CHOICES, default=ATTACHED)
     yard_fenced = models.BooleanField('Is the yard fenced?', default=True)
-    price = models.DecimalField(max_digits=10, decimal_places=0)
 
     def __str__(self):
         return f'{self.square_footage}sqft-{self.num_stories}stories-${self.price}'
@@ -58,7 +68,6 @@ class Apartment(Property):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     balcony = models.BooleanField('Has a balcony?', default=True)
     laundry = models.CharField(max_length=2, choices=LAUNDRY_CHOICES, default=ENSUITE)
-    price = models.DecimalField(max_digits=10, decimal_places=0)
 
     def __str__(self):
         return f'{self.square_footage}sqft-${self.price}'
